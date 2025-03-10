0ee6d6092e7a6dd6e37c43668064bba4d65bb05d
Ryotarou Shiozawa <pekoemood@gmail.com>
2025-03-10 23:42:58 +0900

    Reactチュートリアル学習開始7日目

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,26 @@ let nextId = 0
 const App = () => {
   const [todos, setTodos] = useState([])
   const [text, setText] = useState();
-  function addTask(e) {
-    setTodos((pres) => [...pres, {id: nextId++, task: e.target.value}])
-    console.log(todos);
+  const [isAcitve, setIsActive] = useState(false);
+  function addTask() {
+    setTodos((pres) => [...pres, {id: nextId++, task: text}])
+    setText('');
   }
 
-  function inputText() {
-    
+  function input(e) {
+    setText(e.target.value);
+  }
+
+  function deleteTask(todoId) {
+    setTodos((pres) => {
+      return pres.filter((todo) => todo.id !== todoId)
+  })
+}
+
+  function editTask(todoId, e) {
+    setTodos((prevs) => {
+      return prevs.map((todo) => todo.id === todoId ? {...todo, task: e.target.value} : todo)
+    })
   }
 
 
@@ -18,18 +31,19 @@ const App = () => {
     <>
     <div className="container mx-auto text-center">
       <h1 className="text-4xl my-8">Todoアプリ</h1>
-      <input value={text} type="text" placeholder="ここにタスクを入力" className="input mb-8" />
-      <button onClick={inputText} className="btn mb-8">タスクに追加</button>
+      <input value={text} onChange={(e) => input(e)} type="text" placeholder="ここにタスクを入力" className="input mb-8" />
+      <button onClick={addTask} className="btn mb-8">タスクに追加</button>
       <ol>
-        {todos.map((todo) => (
-          <li className="text-2xl mb-4">{todo.task}</li>
-        )
-        )}
+        {todos.map((todo) => 
+          <div className="flex justify-center space-x-4">
+            { isAcitve ? <input value={todo.task} onChange={(e) => editTask(todo.id, e)} type="text" placeholder="ここにタスクを入力" className="input mb-8" /> :
+                                 <li key={todo.id} className="text-2xl mb-4">{todo.task}</li>}
+            <button className="btn" onClick={() => setIsActive(!isAcitve)}>{isAcitve ? '保存' : '編集'}</button>
+            <button className="btn" onClick={() => deleteTask(todo.id)}>削除</button>
+          </div>)
+        }
       </ol>
     </div>
-
-
-
     </>
 
   )
